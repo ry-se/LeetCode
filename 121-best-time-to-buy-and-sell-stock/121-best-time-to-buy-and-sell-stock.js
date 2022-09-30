fc8df97b8fd35f25f2a0d1ef4fe5f0920b4a2f83
ry-se <87013800+ry-se@users.noreply.github.com>
2022-09-30 13:29:41 +0800

    Time: 1909 ms (5.02%), Space: 52.1 MB (11.64%) - LeetHub

---
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+    let min = prices[0];
+    let profit = 0;
+    
+    for (var i = 1; i < prices.length; i++) {
+        min = Math.min(min, prices[i-1])
+        profit = Math.max(prices[i] - min, profit)
+        if (prices.indexOf(min) === prices.length - 1) {
+            return 0
+        }
+    }
+    console.log(min, profit)
+    return profit;
+};
